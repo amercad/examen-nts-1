bcec8690fae33e640b90c02e679593be1c4612a6
209 <mgilma018@gmail.com>
2022-09-10 11:09:38 -0500

    NTS6-Se creo el punto 2 del examen de pyton

---
+++ punto2.py
@@ -0,0 +1,21 @@
+frutas = []
+
+
+for i in range(2):
+    fruta = {}
+    fruta['nombre'] = input('Digite el nombre de la fruta: ')
+    fruta['color'] = input('Digite el color de la fruta: ')
+    fruta['precio'] = input('Digite el precio de la fruta: ')
+
+    frutas.append( fruta )
+
+    print()
+
+
+contInverso = len(frutas) 
+
+
+
+for i in range(2):
+    print(f'FRUTA {i}: { frutas[contInverso - 1] }')
+    contInverso -= 1
